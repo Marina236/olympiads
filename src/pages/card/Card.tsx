35fa9d09e1141e599aee85f16ddba75f9eda2cb3
CORP\m.vinogradova <marina-vinogradova-99@inbox.ru>
2025-06-18 14:19:19 +0300

    achievement icons

--- src/pages/card/Card.tsx
+++ src/pages/card/Card.tsx
@@ -1,10 +1,14 @@
-import {Flex, Grid, GridItem, Heading, Text, Card as CardChakra, Avatar, Table, Breadcrumb} from "@chakra-ui/react"
+import {Flex, Grid, GridItem, Heading, Text, Image, Card as CardChakra, Avatar, Table, Breadcrumb} from "@chakra-ui/react"
 import React from 'react';
 import Moment from 'moment';
 import {useNavigate, useParams} from "react-router-dom";
 import {rating} from "../../mocks/rating";
 import {useUnit} from "effector-react/compat";
 import {$profile} from "../../components/user/model";
+import gold from '../../image/gold.png';
+import silver from '../../image/silver.png';
+import bronze from '../../image/bronze.png';
+import certificate from '../../image/certificate.png';
 
 export const Card = () => {
     const {id, childId} = useParams<{id: string; childId: string}>();
@@ -16,6 +20,21 @@ export const Card = () => {
 
     if (!classItem  || !childItem) {
       return <Text>Ученик не найден</Text>
+    }
+
+    const getImage = (type: string) => {
+        switch (type) {
+            case 'gold':
+              return gold
+            case 'silver':
+                return silver
+            case 'bronze':
+                return bronze
+            case 'certificate':
+                return certificate
+            default:
+                break;
+        }
     }
 
   return (
@@ -42,33 +61,43 @@ export const Card = () => {
           <Grid templateColumns="repeat(3, 1fr)" gap="6">
             <GridItem colSpan={1}>
               <Heading mb="10px">Список наград</Heading>
-              <Flex gap="10px" direction="column">
-                  {childItem?.awards?.length ? (
-                      <>
-                        {childItem?.awards?.map((item, index) => (
-                            <CardChakra.Root variant='elevated' key={'award' + index}>
-                                <Flex padding="10px" direction="row" gap="1" alignItems="center" justifyContent="space-between">
-                                    <Flex padding="10px" gap="1" direction="column">
-                                        <CardChakra.Title>{item.title}</CardChakra.Title>
-                                        <CardChakra.Description color="fg.muted">
-                                            {item.description}
-                                        </CardChakra.Description>
-                                    </Flex>
-                                    <Avatar.Root>
-                                        <Avatar.Fallback name={item.type} />
-                                        <Avatar.Image src={item.type} />
-                                    </Avatar.Root>
-                                </Flex>
-                            </CardChakra.Root>
-                        ))}
-                      </>
-                  ) : (<Text>Нет наград</Text>)}
-              </Flex>
-            </GridItem>
-            <GridItem colSpan={2}>
-              <Heading mb="10px">История участия</Heading>
+                <Flex gap="10px" direction="column">
+                    {childItem?.awards?.length ? (
+                        <>
+                            {childItem?.awards?.map((item, index) => (
+                                <CardChakra.Root variant='elevated' key={'award' + index}>
+                                    <Flex padding="10px" direction="row" gap="1" alignItems="center"
+                                          justifyContent="space-between">
+                                        <Flex padding="10px" gap="1" direction="column">
+                                            <CardChakra.Title>{item.title}</CardChakra.Title>
+                                            <CardChakra.Description color="fg.muted">
+                                                {item.description}
+                                            </CardChakra.Description>
+                                        </Flex>
 
-                <Table.ScrollArea borderWidth="1px" rounded="md">
+                                        <Image
+                                            src={getImage(item.type)}
+                                            w="40px"
+                                            h='40px'
+                                            objectFit="contain"
+                                            rounded="100%"
+                                        />
+
+                                        {/*<Avatar.Root>*/}
+                                        {/*    <Avatar.Fallback name={item.type}/>*/}
+                                        {/*    <Avatar.Image src={getImage(item.type)}/>*/}
+                                        {/*</Avatar.Root>*/}
+                                    </Flex>
+                                </CardChakra.Root>
+                            ))}
+                        </>
+                    ) : (<Text>Нет наград</Text>)}
+                </Flex>
+            </GridItem>
+              <GridItem colSpan={2}>
+                  <Heading mb="10px">История участия</Heading>
+
+                  <Table.ScrollArea borderWidth="1px" rounded="md">
                     <Table.Root size="sm" stickyHeader>
                         <Table.Header>
                             <Table.Row bg="bg.subtle">
